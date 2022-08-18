b276cc83c4586801c1787e1dec2748d5c9f0b973
Hasibur Rahman <81801818+hrtamim@users.noreply.github.com>
2022-08-18 22:44:25 +0600

    ok

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import {textColor,primaryColor} from './../assets/colors'
 
 const Home = () => {
     return (
         <View>
-            <Text>Home</Text>
+            <Text style={{color:textColor}}>Home</Text>
         </View>
     );
 };
