49d65350b77ab7fefe705cb8b9041a9ad7395172
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-08-18 22:18:56 +0600

    initial

---
+++ components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
+
+const Header = () => {
+    return (
+        <View style={styles.box}>
+           <Text style={styles.text}>Dutypedia</Text>
+           <TouchableOpacity style={styles.input}>
+           <EvilIcons style={{
+            marginRight:10
+           }} name="search" size={24} color="black" />
+           <Text>Search On Dutypedia</Text>
+           </TouchableOpacity>
+        </View>
+    );
+};
+
+export default Header;
+const styles = StyleSheet.create({
+    box:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:30
+    },
+    text:{
+        color: '#000000',
+        fontSize:20,
+        fontWeight:'bold'
+    },
+    input:{
+        margin:20,
+        backgroundColor:'#ffff',
+        height:40,
+        width:'90%',
+        borderRadius:5,
+        flexDirection:'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
---
+++ screens/TabRoute.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './Home';
+import Header from './../components/Header';
+import Search from './Search';
+import Message from './Message';
+import Notification from './Notification';
+import Profile from './Profile';
+import { Ionicons } from '@expo/vector-icons';
+const Tab = createBottomTabNavigator();
+
+const TabRoute = () => {
+    return (
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search-sharp' : 'search-outline';
+            }
+            else if (route.name === 'Message') {
+              iconName = focused ? 'paper-plane-sharp' : 'paper-plane-outline';
+            }
+            else if (route.name === 'Notification') {
+              iconName = focused ? 'notifications-sharp' : 'notifications-outline';
+            }
+            else if (route.name === 'Profile') {
+              iconName = focused ? 'person-circle-sharp' : 'person-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#808080',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen options={{header:()=><Header/>}} name="Home" component={Home} />
+        <Tab.Screen options={{header:()=><Header/>}} name="Search" component={Search} />
+        <Tab.Screen options={{header:()=><Header/>}} name="Message" component={Message} />
+        <Tab.Screen options={{header:()=><Header/>}} name="Notification" component={Notification} />
+        <Tab.Screen options={{header:()=><Header/>}} name="Profile" component={Profile} />
+      </Tab.Navigator>
+    );
+};
+
+export default TabRoute;
