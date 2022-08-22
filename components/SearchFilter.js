ecb279b6294d6ec346975e3a4d6c28835199c79f
Hasibur Rahman <81801818+hrtamim@users.noreply.github.com>
2022-08-22 21:24:07 +0600

    ok

--- components/SearchFilter.js
+++ components/SearchFilter.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Switch,TouchableOpacity,TextInput } from 'react-native';
-import { primaryColor,secondaryColor } from '../assets/colors';
+import { View, Text, StyleSheet, Switch, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import { primaryColor, secondaryColor } from '../assets/colors';
 import { AntDesign } from '@expo/vector-icons';
 
 
@@ -19,6 +19,7 @@ const SearchFilter = () => {
                 alignItems: 'center',
                 justifyContent: 'center',
                 height: 30,
+                
             }}>
                 <Text style={{
                     fontSize: 14,
@@ -28,109 +29,160 @@ const SearchFilter = () => {
                 </Text>
 
             </View>
-            <View style={styles.box}>
-                <Text style={{
-                    fontWeight:'bold'
-                }}>
-                    Sort By
-                </Text>
-            </View>
-            <View style={styles.box1}>
-                <Text>
-                    Online Seller
-                </Text>
-            </View>
-            <View style={{
-                backgroundColor: secondaryColor,
-                height:2
-            }}>
+            <ScrollView>
 
-            </View>
-            <View style={[styles.box1,{ flexDirection:'row',}]}>
-                <Text style={{
-                    flex:7
-        
-                }}>
-                    Verified Seller
-                </Text>
-                <View style={styles.container}>
-                    <Switch
-                        trackColor={{ false: "#767577", true: "#808000" }}
-                        thumbColor={isEnabled ? "#f4f3f4 " : "#f4f3f4"}
-                        ios_backgroundColor="#3e3e3e"
-                        onValueChange={toggleSwitch}
-                        value={isEnabled}
-                    />
-                </View>
-            </View>
-            <View style={{
-                backgroundColor: secondaryColor,
-                height:2
-            }}>
-
-            </View>
-            <View style={styles.box}>
-                <Text style={{
-                    fontWeight:'bold'
-                }}>
-                    Filter
-                </Text>
-            </View>
-            <View style={{
-                backgroundColor: secondaryColor,
-                height:2
-            }}>
-
-            </View>
-            <View style={[styles.box1,{ flexDirection:'row'}]}>
-                <Text style={{
-                    flex:9
-                }}>
-                    Category
-                </Text>
-                <TouchableOpacity style={{
-                    flexDirection:'row',
-                    justifyContent:'center',
-                    alignItems:'center',
-                    flex:3
-                }}>
+                <View style={styles.box}>
                     <Text style={{
-                        marginRight:20,
-                        flex:2
+                        fontWeight: 'bold'
                     }}>
-                        Select
+                        Sort By
                     </Text>
-                    <AntDesign style={{
-                    marginTop:5,
-                }} name="right" size={16} color="black" />
-                </TouchableOpacity>
-                
-            </View>
-            <View style={styles.box}>
-                <Text style={{
-                    fontWeight:'bold'
+                </View>
+                <View style={{
+                    height: 50,
+                    marginLeft: 10,
+                    backgroundColor: '#ffffff',
+                    justifyContent: 'center',
                 }}>
-                    Filter
-                </Text>
-            </View>
-            <View style={[styles.box1,{ flexDirection:'row'}]}>
-                <Text style={{
-                    flex:9
-                }}>
-                    Price Rang
-                </Text>
-                <TextInput style={{
-                    height:30,
-                    width:140,
-                    borderWidth:1
+                    <Text>
+                        Online Seller
+                    </Text>
+                </View>
+                <View style={styles.gap}></View>
+                <View style={[styles.box1, { flexDirection: 'row', }]}>
+                    <Text style={{
+                        flex: 7
+
+                    }}>
+                        Verified Seller
+                    </Text>
+                    <View style={styles.container}>
+                        <Switch
+                            trackColor={{ false: "#767577", true: "#808000" }}
+                            thumbColor={isEnabled ? "#f4f3f4 " : "#f4f3f4"}
+                            ios_backgroundColor="#3e3e3e"
+                            onValueChange={toggleSwitch}
+                            value={isEnabled}
+                        />
+                    </View>
+                </View>
+                <View style={{
+                    backgroundColor: secondaryColor,
+                    height: 2
                 }}>
 
-                </TextInput>
-                
-                
-            </View>
-            
-            
+                </View>
+                <View style={styles.box}>
+                    <Text style={{
+                        fontWeight: 'bold'
+                    }}>
+                        Filter
+                    </Text>
+                </View>
+                <View style={{
+                    backgroundColor: secondaryColor,
+                    height: 2
+                }}>
+
+                </View>
+                <View style={[styles.box1, { flexDirection: 'row' }]}>
+                    <Text style={{
+                        flex: 9
+                    }}>
+                        Category
+                    </Text>
+                    <TouchableOpacity style={{
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flex: 3
+                    }}>
+                        <Text style={{
+                            marginRight: 20,
+                            flex: 2
+                        }}>
+                            Select
+                        </Text>
+                        <AntDesign style={{
+                            marginTop: 5,
+                        }} name="right" size={16} color="black" />
+                    </TouchableOpacity>
+
+                </View>
+                <View style={styles.box}>
+                    <Text style={{
+                        fontWeight: 'bold'
+                    }}>
+                        Filter
+                    </Text>
+                </View>
+                <View style={[styles.box1, { flexDirection: 'row' }]}>
+                    <Text style={{
+                        flex: 4
+                    }}>
+                        Price Rang
+                    </Text>
+                    <TextInput style={styles.input}>
+                        <Text>
+                            $
+                        </Text>
+                    </TextInput>
+                    <View style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <Text style={{
+                            marginLeft: 10,
+                            marginRight: 10,
+                        }}>
+                            To
+                        </Text>
+                    </View>
+                    <TextInput style={styles.input}>
+                        <Text style={{
+                        }}>
+                            $
+                        </Text>
+                    </TextInput>
+
+
+                </View>
+                <View style={styles.gap}></View>
+                <View style={[styles.box1, { flexDirection: 'row' }]}>
+                    <Text style={{
+                        flex: 9
+                    }}>
+                        Seller Level
+                    </Text>
+                    <TouchableOpacity style={styles.touch}>
+
+                        <AntDesign style={{
+                            marginTop: 5,
+                            marginLeft: 18
+                        }} name="right" size={16} color="black" />
+                    </TouchableOpacity>
+
+                </View>
+                <View style={styles.gap}></View>
+                <View style={[styles.box1, { flexDirection: 'row' }]}>
+                    <Text style={{
+                        flex: 9,
+
+                    }}>
+                        Seller Location
+                    </Text>
+                    <TouchableOpacity style={styles.touch}>
+
+                        <AntDesign style={{
+                            marginTop: 5,
+                            marginLeft: 18
+                        }} name="right" size={16} color="black" />
+                    </TouchableOpacity>
+
+                </View>
+            </ScrollView>
+
+
         </View>
     );
 };
@@ -139,7 +191,7 @@ export default SearchFilter;
 
 const styles = StyleSheet.create({
     box: {
-        height: 30,
+        height: 50,
         backgroundColor: '#fbfbfb',
         justifyContent: "center"
 
@@ -149,13 +201,32 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
-    box1:{
+    box1: {
+        height: 50,
+        marginLeft: 10,
+        marginRight: 10,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+
+    },
+    input: {
         height: 30,
-                marginLeft: 10,
-                marginRight: 10,
-                backgroundColor: '#ffffff',
-                justifyContent:'center',
-            
+        width: 120,
+        borderWidth: 1,
+        flex: 3,
+        borderRadius: 5
+    },
+    touch: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    gap: {
+        backgroundColor: secondaryColor,
+        height: 2
     }
 
 
