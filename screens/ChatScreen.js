80c7127c67873da45c062c796facdd493915c4be
Hasibur Rahman <81801818+hrtamim@users.noreply.github.com>
2022-08-22 15:42:11 +0600

    ok

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,19 @@
 
-import React from 'react';
-import { View, Text, ScrollView, Dimensions } from 'react-native'
-import ChatBox from './../components/ChatBox';
+import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Dimensions,
+  TextInput,   
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import ChatBox from "./../components/ChatBox";
+import { EvilIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { backgroundColor, assentColor, primaryColor,secondaryColor } from "./../assets/colors";
+const { width, height } = Dimensions.get("window");
 
 
 const ChatScreen = () => {
