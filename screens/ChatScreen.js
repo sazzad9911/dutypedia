41c30727cc808688b63531843b2e4c0cb1c7425f
Hasibur Rahman <81801818+hrtamim@users.noreply.github.com>
2022-08-22 15:44:16 +0600

    ok

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -14,6 +14,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { backgroundColor, assentColor, primaryColor,secondaryColor } from "./../assets/colors";
 const { width, height } = Dimensions.get("window");
 
+
 const ChatScreen = () => {
   const scrollRef = React.useRef();
   const [Messages, setMessages] = React.useState([
