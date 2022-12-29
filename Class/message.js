eec66a3b89422fe7ad9ddb7ed4c62c27ca58da57
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2022-12-29 10:23:34 +0600

    okk

--- Class/message.js
+++ Class/message.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { url } from "../action";
 
 export const createConversation=async(token,username)=>{
-    const res=await axios.get(`${url}server/chat/conversation/create?username=${username}`,{
+    const res=await axios.get(`${url}/server/chat/conversation/create?username=${username}`,{
         headers:{ Authorization: `Bearer ${token}` }
     })
     return res
