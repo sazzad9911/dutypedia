d2ec2738d220d52b3ef287db698b0d022f989372
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2023-05-24 19:38:43 +0600

    dfg

--- Class/service.js
+++ Class/service.js
@@ -212,12 +212,7 @@ export const createOtherServiceIndividual = async (
         ? installmentData.totalAmount
         : price
     ),
-    facilites: {
-      title: "Choose Your Facilities",
-      selectedOptions: Array.isArray(businessForm.facilities)
-        ? businessForm.facilities.filter((data) => data.checked == true)
-        : [],
-    },
+    facilites: businessForm.facilities,
     services: {
       category: getDashboardTitle(listData[0].mainTitle),
       type: listData[0].subTitle ? 3 : listData[0].title ? 2 : 1,
